2e12ac6ec3b28a80c2bc657cd6a4d4a3213f7ead
Peter Keating <peter@moov2.com>
2013-01-24 21:11:16 +0000

    added some comments to the r.js build configuration file.

--- src/app/js/build.js
+++ src/app/js/build.js
@@ -1,6 +1,17 @@
 ({
+    /**
+    * Specifies the location of the runtime configuration file that instructs
+    * requireJS how to construct the application.
+    */
     mainConfigFile: "./config.js",
+
+    /**
+    * Name of the module to be optimised.
+    */
     name: "main",
-    preserveLicenseComments: false,
+
+    /**
+    * File that is outputted by r.js containing all the code for the application.
+    */
     out: "../../../intermediate/js/main-optimised.js"
 })
