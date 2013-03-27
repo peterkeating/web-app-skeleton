2c91ce8dba0fe56a71c28307403c32528eca9f19
Peter Keating <peter@moov2.com>
2013-03-27 18:18:07 +0000

    created the most basic grunt file which runs but does nothing.

---
+++ gruntfile.js
@@ -0,0 +1,17 @@
+'use strict';
+
+module.exports = function(grunt) {
+
+    /**
+     * Configuring the tasks available for the build process.
+     */
+    grunt.initConfig({
+
+    });
+
+    /**
+     * Register the default task that will be responsible for building the web
+     * application.
+     */
+    grunt.registerTask('default', []);
+};
