0dfb15e98c3760f4efb99ff1c92e6b87689360e0
Peter Keating <peter@moov2.com>
2013-03-27 22:50:15 +0000

    grunt build script uses the r.js optimizer to create a single file containing the entired JS for the application.

--- gruntfile.js
+++ gruntfile.js
@@ -7,11 +7,33 @@ module.exports = function(grunt) {
      */
     grunt.initConfig({
 
+        /**
+         * Optimizes the JavaScript into a single file using the r.js optimizer.
+         */
+        requirejs: {
+            compile: {
+                options: {
+                    name: 'main',
+                    mainConfigFile: 'src/app/js/config.js',
+                    paths: {
+                        requireJS: '../../vendor/require'
+                    },
+                    include: 'requireJS',
+                    preserveLicenseComments: false,
+                    out: 'dist/js/app.js'
+                }
+            }
+        }
     });
+
+    /**
+     * Loads libraries to assist with the build process.
+     */
+    grunt.loadNpmTasks('grunt-contrib-requirejs');
 
     /**
      * Register the default task that will be responsible for building the web
      * application.
      */
-    grunt.registerTask('default', []);
+    grunt.registerTask('default', ['requirejs']);
 };
